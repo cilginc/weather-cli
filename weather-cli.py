30f19262296078213ad112060aa855afd32789bb
cilgin <104676201+cilginc@users.noreply.github.com>
2024-12-26 21:07:15 +0300

    Add files via upload

---
+++ weather-cli.py
@@ -0,0 +1,40 @@
+import requests
+
+def inputwhere():
+    return input("Search for a place? (x for exit): ")
+
+def getweather():
+    getinput = inputwhere()
+    if getinput == "x":
+        return "Exited"
+    else:
+        url = f"https://wttr.in/{getinput}?format=j1"
+        response = requests.get(url)
+        if response.status_code == 200:
+           data = response.json()
+           current_condition = data.get('current_condition', [])[0]
+           nearest_area = data.get('nearest_area', [])[0]
+           print("------------------------------")
+           print("Current Weather:")
+           print(f"Temp: {current_condition.get('temp_C')}°C")
+           print(f"Feels Like Temp: {current_condition.get('FeelsLikeC')}°C")
+           print(f"Humidity: {current_condition.get('humidity')}%")
+           print(f"Wind Speed: {current_condition.get('windspeedKmph')} km/s")
+           print(f"Weather Description: {current_condition.get('weatherDesc')[0].get('value')}")
+           print("Location:")
+           print(f"Area: {nearest_area.get('areaName')[0].get('value')}")
+           print(f"Region: {nearest_area.get('region')[0].get('value')}")
+           print(f"Country: {nearest_area.get('country')[0].get('value')}")
+           print("-----------------------------")
+           return ""
+        elif response.status_code == 404:
+            return f"There is nowhere named {getinput} "
+        else:
+            return "Something went wrong"
+
+if __name__ == "__main__":
+    while True:
+        weather = getweather()
+        if weather == "Exited":
+            break
+
